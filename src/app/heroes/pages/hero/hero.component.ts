171dc6aa71cb4d12befaa8c7a773be1d10f33f6d
Gino Ascencio <gino.francisco@hotmail.com>
2022-11-06 12:27:43 -0500

    add activatedroute and router link

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => console.log(params['id']));
   }
 
 }
