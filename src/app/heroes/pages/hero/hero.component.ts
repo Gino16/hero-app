382b297eb1db4384ffe1afe877f46463e81331f5
Gino Ascencio <gino.francisco@hotmail.com>
2022-11-06 14:09:03 -0500

    add card hero

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { Hero } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
@@ -7,19 +7,25 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+    }
+  `]
 })
 export class HeroComponent implements OnInit {
 
   hero!: Hero;
 
-  constructor(private route: ActivatedRoute, private heroesService: HeroesService) { }
+  constructor(private route: ActivatedRoute, private heroesService: HeroesService, private router: Router) {}
 
   ngOnInit(): void {
     this.route.params.pipe(
-      switchMap(({id}) => this.heroesService.getHeroById(id))
+      switchMap(({ id }) => this.heroesService.getHeroById(id))
     ).subscribe(res => this.hero = res);
   }
 
+  regresar() {
+    this.router.navigate(['/heroes/list']);
+  }
 }
